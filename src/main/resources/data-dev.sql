5e26ad17748d0b5d86f19f7f2d5ef33f2f260c03
JabKhan23 <107106989+JabKhan23@users.noreply.github.com>
2022-08-11 00:04:57 +0100

    renamed test and included test dbs

---
+++ src/main/resources/data-dev.sql
@@ -0,0 +1,11 @@
+INSERT INTO TO_DO_LISTS_DOMAIN (TITLE)
+VALUES
+('General Tasks'),
+('Hit List'),
+('QA Project 2 tasks');
+
+INSERT INTO TO_DO_ENTRIES_DOMAIN (DESCRIPTION,DUE_DATE, COMPLETED, MY_LIST_ID)
+VALUES
+('create back end and cry','2022-08-10',true,3),
+('testing for back end and also cry','2022-08-10',false,3),
+('create front end and finally cry','2022-08-10',false,3),
